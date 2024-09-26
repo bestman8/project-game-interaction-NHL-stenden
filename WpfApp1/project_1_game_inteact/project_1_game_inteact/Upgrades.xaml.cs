dc54573fe7a9555f02e419c684cd7307c822cb6c
Sven Terpstr <sven.terpstra1@student.nhlstenden.com>
2024-09-26 11:10:55 +0200

    Upgrades menu toegevoegd

---
+++ WpfApp1/project_1_game_inteact/project_1_game_inteact/Upgrades.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using UItest;
+
+namespace project_1_game_inteact
+{
+    /// <summary>
+    /// Interaction logic for Upgrades.xaml
+    /// </summary>
+    public partial class Upgrades : Window
+    {
+        public Upgrades()
+        {
+            InitializeComponent();
+        }
+
+        private void LevelsClick(object sender, RoutedEventArgs e)
+        {
+            levels gm = new levels();
+            gm.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+
+        private void StartschermClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow gm = new MainWindow();
+            gm.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
