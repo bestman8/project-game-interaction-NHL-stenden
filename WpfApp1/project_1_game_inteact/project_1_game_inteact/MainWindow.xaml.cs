280f2749a115339ac4f3fc8e066b2bddb6ac14df
SvenHiemstra <sven.hiemstra1@student.nhlstenden.com>
2024-10-11 09:19:55 +0200

    poging 3

--- WpfApp1/project_1_game_inteact/project_1_game_inteact/MainWindow.xaml.cs
+++ WpfApp1/project_1_game_inteact/project_1_game_inteact/MainWindow.xaml.cs
@@ -21,56 +21,8 @@ namespace UItest
     {
         public MainWindow()
         {
-            InitializeComponent();
-
-            multi.Content = "Alleen spelen";
-         
-        }
-        private int j = 0;
-        private void multi_Click(object sender, RoutedEventArgs e)
-        {
-
-       
-            if (j == 1)
-            {
-                multi.Content = "Alleen spelen";
-                j = 0;
-                speler1.Visibility = Visibility.Visible;
-                speler2.Visibility = Visibility.Visible;
-                lspeler1.Visibility = Visibility.Visible;   
-                lspeler2.Visibility = Visibility.Visible;
-                speler1.Text = "";
-                speler2.Text = "";
-            }
-            else if (j == 0)
-            {
-                multi.Content = "Samen spelen";
-                j = 1;
-                speler1.Visibility = Visibility.Visible;        
-                lspeler1.Visibility = Visibility.Visible;
-                lspeler2.Visibility = Visibility.Hidden;
-                speler2.Visibility = Visibility.Hidden;
-                speler1.Text = "";
-                speler2.Text = "";
-               
-            }
-
-        }
-
-        private void levels_Click(object sender, RoutedEventArgs e)
-        {
-
-
-            this.Hide();
-
-
-            UItest.levels secondWindow = new UItest.levels();
-            secondWindow.Show();
-
-
-        }
-
-    
+            InitializeComponent();    
+        }   
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.Hide();
