c6ef26ef549f4825b0f039c3721cafcafc7012ac
SvenHiemstra <sven.hiemstra1@student.nhlstenden.com>
2024-10-11 10:14:29 +0200

    poging 4

--- WpfApp1/project_1_game_inteact/project_1_game_inteact/MainWindow.xaml.cs
+++ WpfApp1/project_1_game_inteact/project_1_game_inteact/MainWindow.xaml.cs
@@ -30,5 +30,49 @@ namespace UItest
             start.Show();
 
         }
+
+        private void levels_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+
+
+            UItest.levels secondWindow = new UItest.levels();
+            secondWindow.Show();
+        }
+
+        private int j = 0;
+        private void multi_Click(object sender, RoutedEventArgs e)
+        {
+            if (j == 1)
+            {
+                multi.Content = "Alleen spelen";
+                j = 0;
+                speler1.Visibility = Visibility.Visible;
+                speler2.Visibility = Visibility.Visible;
+                lspeler1.Visibility = Visibility.Visible;
+                lspeler2.Visibility = Visibility.Visible;
+                speler1.Text = "";
+                speler2.Text = "";
+            }
+            else if (j == 0)
+            {
+                multi.Content = "Samen spelen";
+                j = 1;
+                speler1.Visibility = Visibility.Visible;
+                lspeler1.Visibility = Visibility.Visible;
+                lspeler2.Visibility = Visibility.Hidden;
+                speler2.Visibility = Visibility.Hidden;
+                speler1.Text = "";
+                speler2.Text = "";
+            }
+
+        }
+
+        private void upgrades_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Upgrades upgradesWindow = new Upgrades();
+            upgradesWindow.Show();
+        }
     }
 }
