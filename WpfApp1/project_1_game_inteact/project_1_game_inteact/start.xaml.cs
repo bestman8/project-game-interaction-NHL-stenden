092e68fa09d198c2b19826bb1f9bd39c0f95d532
Nikoo! <nikhiel.van.der.veen@student.nhlstenden.com>
2024-10-22 11:16:15 +0200

    levels

--- WpfApp1/project_1_game_inteact/project_1_game_inteact/start.xaml.cs
+++ WpfApp1/project_1_game_inteact/project_1_game_inteact/start.xaml.cs
@@ -85,6 +85,7 @@ namespace project_1_game_inteact
             public double max_Speed { get; set; }
             public double acceleration {  get; set; }
             public double gravity { get; set; }
+            public int levels { get; set; }
         }
         private void startinf()
         {
@@ -97,6 +98,7 @@ namespace project_1_game_inteact
                 SharedData.Instance.Geld2 = 50;
                 SharedData.Instance.acceleration = 0.1;
                 SharedData.Instance.gravity = 0.00051; //0.01 is natural ish
+                SharedData.Instance.levels = 1;
 
     }
         }
