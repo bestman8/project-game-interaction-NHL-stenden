e200dc130c31fb5f818e408ffbd5466ed469e14c
Sven Terpstr <sven.terpstra1@student.nhlstenden.com>
2024-10-22 10:31:57 +0200

    start knop werkt nu better, namen toegevoed als niks is ingevuld, info knoppen werken

--- WpfApp1/project_1_game_inteact/project_1_game_inteact/start.xaml.cs
+++ WpfApp1/project_1_game_inteact/project_1_game_inteact/start.xaml.cs
@@ -97,8 +97,10 @@ namespace project_1_game_inteact
                 SharedData.Instance.Geld2 = 50;
                 SharedData.Instance.acceleration = 0.1;
                 SharedData.Instance.gravity = 0.00051; //0.01 is natural ish
+                SharedData.Instance.Naam1 = "Speler 1";
+                SharedData.Instance.Naam2 = "Speler 2";
 
-    }
+            }
         }
 
         private void nspeler2_KeyUp(object sender, KeyEventArgs e)
