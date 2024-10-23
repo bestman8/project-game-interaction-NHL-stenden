15d9fedf5ff8ae2e1b989352603afccdca73b36f
doome <jelle.brans@student.nhlstenden.com>
2024-10-23 21:45:43 +0200

    Handleiding toegevoegd + de stijl van tekst in knoppen aangepast op sommige plekken.

--- WpfApp1/project_1_game_inteact/project_1_game_inteact/handleiding.xaml.cs
+++ WpfApp1/project_1_game_inteact/project_1_game_inteact/handleiding.xaml.cs
@@ -36,19 +36,19 @@ namespace project_1_game_inteact
         private void bewegen_Click(object sender, RoutedEventArgs e)
         {
         hnd_txt.Content = ""; // maakt de string leeg zodra er op de knop wordt gedrukt van de handleiding
-        hnd_txt.Content = ""; // handleiding van het spel
+        hnd_txt.Content = "Hier is een uitleg over hoe je de auto's in het spel kunt laten bewegen.\r\n\r\nDe Auto aan de linkerkant van het scherm:\r\nDruk op de knop A om de auto naar links te laten rijden.\r\nDruk op de knop D om de auto naar rechts te laten rijden.\r\n\r\nDe auto aan de rechterkant van het scherm:\r\nDruk op de pijl naar links (←) om de auto naar links te laten rijden.\r\nDruk op de pijl naar rechts (→) om de auto naar rechts te laten rijden."; // handleiding van de besturing
         }
 
-        private void upgrades_Click(object sender, RoutedEventArgs e)
+        private void spelen_Click(object sender, RoutedEventArgs e)
         {
             hnd_txt.Content = ""; // maakt de string leeg zodra er op de knop wordt gedrukt van de handleiding
-            hnd_txt.Content = ""; // handleiding van het upgraden
+            hnd_txt.Content = "Voordat je kunt beginnen met racen, moet je eerst je naam invullen.\r\nSpeler 1 vult zijn of haar naam in voor de linker auto.\r\nSpeler 2 vult zijn of haar naam in voor de rechter auto.\r\n\r\nHoe werkt de race?\r\nJe auto begint bij de start.\r\nHet doel is om zo snel mogelijk naar de finish te komen!\r\nBovenin het scherm zie je de tijd lopen.\r\nZodra je over de finish komt, stopt je auto vanzelf."; // handleiding van het spel
         }
 
-        private void instellingen_Click(object sender, RoutedEventArgs e)
+        private void punten_Click(object sender, RoutedEventArgs e)
         {
             hnd_txt.Content = ""; // maakt de string leeg zodra er op de knop wordt gedrukt van de handleiding
-            hnd_txt.Content = ""; // handleiding van de instellingen
+            hnd_txt.Content = "Hoe verdien je punten?\r\nHoe sneller je bij de finish bent, hoe meer punten je krijgt.\r\n\r\nWat kun je met de punten? \r\nMet de punten kun je leuke upgrades kopen voor je auto, zoals snellere wielen of een coole motor!"; // handleiding van de punten
         }
 
         private void return_Click(object sender, RoutedEventArgs e)
